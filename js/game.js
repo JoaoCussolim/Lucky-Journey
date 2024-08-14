dfd62ae57632fe563b95d8f9e780dfdc6788129f
JoaoCussolim <joaovccussolim@gmail.com>
2024-08-14 18:12:50 -0300

    Camera text
    Camera position and definition

--- js/game.js
+++ js/game.js
@@ -1,5 +1,22 @@
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
+let background = new Image();
+background.src = "./assets/background.png"
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let scale = 4
+
+let scaledCanvas = {
+    width: canvas.width/scale,
+    height: canvas.height/scale
+}
+
+let camera = {
+    x: 0,
+    y: 0
+}
 
 
 let dice = new Dice({
@@ -7,19 +24,21 @@ let dice = new Dice({
 });
 
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
 let animate = () => {
     requestAnimationFrame(animate)
+    ctx.imageSmoothingEnabled = false;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+    ctx.save();
+    ctx.scale(scale, scale);
+    ctx.translate(camera.x, camera.y - scaledCanvas.width - 100);
     ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    ctx.restore();
+
 
     player.update();
-    dice.update();
-    testEnemy.update();
+    //dice.update();
+    //testEnemy.update();
 };
 
 setTimeout(() => {
--- js/player.js
+++ js/player.js
@@ -1,10 +1,53 @@
 class Player extends Sprite{
     constructor({position = {x: 0, y: 0}, dimensions = {width: 0, height: 0}}){
         super({position, dimensions});
+        this.cameraBox = {
+            x: this.position.x,
+            y: this.position.y,
+            width: this.dimensions.width * 2,
+            height: this.dimensions.height * 2
+        };
+    };
+    updateCameraBox(){
+        this.cameraBox = {
+            x: this.position.x - (this.dimensions.width * 1.5),
+            y: this.position.y - (this.dimensions.height * 1.5),
+            width: this.dimensions.width * 4,
+            height: this.dimensions.height * 4
+        };
+    };
+
+    shouldPanCameraVertical(){
+        let cameraBoxRightSide = this.cameraBox.x + this.cameraBox.width
+
+        if(cameraBoxRightSide > canvas.width) {camera.x -= 1}
+
+        if(cameraBoxRightSide > scaledCanvas.width + Math.abs(camera.x)) camera.x -= this.velocity.x
+
+        //if(camera.x < scaledCanvas.width) camera.x -= this.velocity.x        
+    }
+
+    shouldPanCameraHorizontal(){
+        let cameraBoxRightSide = this.cameraBox.y + this.cameraBox.height
+
+        if(cameraBoxRightSide > canvas.height) {camera.y -= 1}
+
+        if(cameraBoxRightSide > scaledCanvas.height + Math.abs(camera.y)){
+            camera.y -= this.velocity.y
+        }
+        //if(camera.y < scaledCanvas.height) camera.y -= this.velocity.y
+        //else if(this.cameraBox.y - this.cameraBox.height < canvas.height) camera.y -= this.velocity.y
+    }
+
+    update(){
+        this.draw();
+        this.shouldPanCameraHorizontal();
+        this.shouldPanCameraVertical();
+        this.updateCameraBox();
     };
 };
 
-let player = new Player({ position: { x: 100, y: 100 }, dimensions: { width: 100, height: 100 } });
+let player = new Player({ position: { x: 500, y: 300 }, dimensions: { width: 100, height: 100 } });
 
 let acceptedKeys = {
     ArrowUp(player) {
