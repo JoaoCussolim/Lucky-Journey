bb408511116d999e10626e63555b64779755bc5a
JoaoCussolim <joaovccussolim@gmail.com>
2024-08-14 11:52:26 -0300

    Update Moviment
    Created player moviment

---
+++ js/player.js
@@ -0,0 +1,49 @@
+class Player extends Sprite{
+    constructor({position = {x: 0, y: 0}, dimensions = {width: 0, height: 0}}){
+        super({position, dimensions})
+    }
+}
+
+let player = new Player({ position: { x: 100, y: 100 }, dimensions: { width: 100, height: 100 } })
+
+let acceptedKeys = {
+    ArrowUp(player) {
+        player.velocity.x = 0;
+        player.velocity.y = -5;
+    },
+    ArrowDown(player) {
+        player.velocity.x = 0;
+        player.velocity.y = 5;
+    },
+    ArrowLeft(player) {
+        player.velocity.y = 0;
+        player.velocity.x = -5;
+    },
+    ArrowRight(player) {
+        player.velocity.y = 0;
+        player.velocity.x = 5;
+    }
+};
+
+let keyUpKeys = {
+    ArrowUp(player) {
+        if(player.velocity.y < 0) player.velocity.y = 0;
+    },
+    ArrowDown(player) {
+        if(player.velocity.y > 0) player.velocity.y = 0;
+    },
+    ArrowLeft(player) {
+        if(player.velocity.x < 0) player.velocity.x = 0;
+    },
+    ArrowRight(player) {
+        if(player.velocity.x > 0) player.velocity.x = 0;
+    }
+}
+
+addEventListener('keydown', (e) => {
+    if(acceptedKeys[e.key]) acceptedKeys[e.key](player)
+})
+
+addEventListener('keyup', (e) => {
+    if(acceptedKeys[e.key]) keyUpKeys[e.key](player)
+})
--- js/sprite.js
+++ js/sprite.js
@@ -1,18 +1,3 @@
-let acceptedKeys = {
-    ArrowUp(player){
-        player.velocity.y = 5;
-    },
-    ArrowDown(player){
-        player.velocity.y = -5;
-    },
-    ArrowLeft(player){
-        player.velocity.x = 5;
-    },
-    ArrowRight(player){
-        player.velocity.x = -5;
-    }
-};
-
 class Sprite{
     constructor({position = {x: 0, y: 0}, dimensions = {width: 0, height: 0}}){
         this.position = position;
@@ -38,9 +23,3 @@ class Sprite{
         this.draw();
     }
 }
-
-let player = new Sprite({position: {x: 100, y: 100}, dimensions: {width: 100, height: 100}})
-
-addEventListener('keydown', (e) => {
-    acceptedKeys(e.key)
-})
