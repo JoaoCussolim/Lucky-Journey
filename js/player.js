91f8f036fae95559d0b2eca4a5115bb3ddff73c5
JoaoCussolim <joaovccussolim@gmail.com>
2024-08-15 10:15:55 -0300

    Mage sprites
    Mage sprites added to the assets folder

--- js/player.js
+++ js/player.js
@@ -32,7 +32,7 @@ class Player extends Sprite{
     };
 };
 
-let player = new Player({ position: { x: 100, y: 500 }, dimensions: { width: 100, height: 100 } });
+let player = new Player({ position: { x: 950, y: 500 }, dimensions: { width: 100, height: 100 } });
 
 let acceptedKeys = {
     ArrowUp(player) {
---
+++ js/projectile.js
@@ -0,0 +1,5 @@
+class Projectile extends Sprite{
+    constructor({ position = { x: 0, y: 0 }, dimensions = { width: 0, height: 0 } }){
+        super(position, dimensions);
+    };
+};
